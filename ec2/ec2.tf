1ff5b8b64cb624e1a1985afe358d34e5f6a9af1a
yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-26 13:53:38 +0530

    changes done

---
+++ ec2/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "terraform"
+  }
+}
+    
+resource "aws_security_group" "allow_tls" {
+name        = "terraform-allow-all"
+description = "Allow TLS inbound traffic and all outbound traffic"
+        egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        }
+
+        ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        }
+
+    tags = {
+        name = "terraform-allow-all"
+    }
+}
